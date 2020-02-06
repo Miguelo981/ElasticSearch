6d6562c9b7f6cdc40b05fa961347b1884a861f7a
alu2018240 <lorito20@hotmail.es>
2020-02-06 17:37:32 +0100

    Implementacion de dependencias

--- src/main/java/controller/ManagerDao.java
+++ src/main/java/controller/ManagerDao.java
@@ -1,53 +1,25 @@
 package controller;
 
-import org.elasticsearch.*;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
-import org.elasticsearch.xpack.core.XPackClient;
-import org.elasticsearch.xpack.core.XPackPlugin;
-import org.elasticsearch.core.watcher.client.WatcherClient;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 
 public class ManagerDao {
 
     public ManagerDao() {
-        // on startup
-
-        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
-                .addTransportAddress(new TransportAddress(InetAddress.getByName("host1"), 9300))
-                .addTransportAddress(new TransportAddress(InetAddress.getByName("host2"), 9300));
-
-// on shutdown
-        client.close();
-
-        TransportClient client = new PreBuiltXPackTransportClient(Settings.builder().put("cluster.name", "myClusterName")
-        ...
-    .build()
-        )
-    .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
-
-        XPackClient xpackClient = new XPackClient(client);
-        WatcherClient watcherClient = xpackClient.watcher();
-
-        Client client = new PreBuiltTransportClient(
-                Settings.builder().put("client.transport.sniff", true)
-                        .put("cluster.name", "elasticsearch").build())
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
-    }
-
-    private TransportClient getElasticClient() {
+        RestHighLevelClient client = new RestHighLevelClient(
+            RestClient.builder(
+                new HttpHost("localhost", 9200, "http"),
+                new HttpHost("localhost", 9201, "http")
+            )
+        );
         try {
-
-            Settings setting = Settings.builder()
-                    .put("cluster.name", elasticPro.getProperty("cluster"))
-                    .put("client.transport.sniff", Boolean.valueOf(elasticPro.getProperty("transport.sniff"))).build();
-
-            client = new PreBuiltTransportClient(setting)
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(elasticPro.getProperty("host")), Integer.valueOf(elasticPro.getProperty("port"))));
-        } catch (UnknownHostException ex) {
-            log.error("Exception occurred while getting Client : " + ex, ex);
+            client.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ManagerDao.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return client;
     }
 }
