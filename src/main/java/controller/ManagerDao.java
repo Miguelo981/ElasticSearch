44ea58a3cd8da5369767e4d5b1cf15b86234459a
ElHoplita <eric.el.hoplita@gmail.com>
2020-02-06 17:33:13 +0100

    Funciona el "add" (index) y el get

--- src/main/java/controller/ManagerDao.java
+++ src/main/java/controller/ManagerDao.java
@@ -1,20 +1,54 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 
 public class ManagerDao {
+    RestHighLevelClient client;
 
     public ManagerDao() {
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http"),
-                        new HttpHost("localhost", 9201, "http")
-                )
+        client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
         );
+    }
+    
+    public void index(){
+        HashMap<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("user", "eric");
+        jsonMap.put("postDate", new Date());
+        jsonMap.put("message", "trying out Elasticsearch");
+        IndexRequest indexRequest = new IndexRequest("posts")
+                .id("1").source(jsonMap).opType(DocWriteRequest.OpType.CREATE);;
+        try {
+            IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        } catch (IOException ex) {
+            Logger.getLogger(ManagerDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
+    }
+    
+    public String get() throws Exception{
+        GetRequest getRequest = new GetRequest(
+        "posts", 
+        "1"); 
+            GetResponse getResponse = client.get(getRequest, RequestOptions.DEFAULT);
+            return getResponse.getSourceAsString();
+        
+    }
+    
+    public void close(){
         try {
             client.close();
         } catch (IOException ex) {
