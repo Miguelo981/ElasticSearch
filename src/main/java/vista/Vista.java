df5bfb4d886c78d13a42c1cc97451778a4e23e56
alu2018262 <polnadalserra@gmail.com>
2020-02-12 16:03:15 +0100

    Modelo acabado, implementación de enums para los modelos.

--- src/main/java/vista/Vista.java
+++ src/main/java/vista/Vista.java
@@ -10,7 +10,7 @@ public class Vista {
     
     public static void main(String[] args) throws Exception{
         ManagerDao mD = new ManagerDao();
-//        mD.index();
+        mD.index();
         String get = mD.get();
         System.out.println(get);
         mD.update();
