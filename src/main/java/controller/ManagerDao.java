0d8c58b4eb60612f36007227298c047114747a64
alu2018240 <lorito20@hotmail.es>
2020-02-12 16:10:53 +0100

    Implementacion del menu inicial e inputaske

--- src/main/java/controller/ManagerDao.java
+++ src/main/java/controller/ManagerDao.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.http.HttpHost;
@@ -11,42 +12,63 @@ import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 
 public class ManagerDao {
+    private static ManagerDao managerDao;
     RestHighLevelClient client;
 
-    public ManagerDao() {
+    private ManagerDao() {
         client = new RestHighLevelClient(
                 RestClient.builder(new HttpHost("localhost", 9200, "http"))
         );
     }
     
-    public void index(){
+    public static ManagerDao getManagerDao() {
+        if (managerDao == null) {
+            managerDao = new ManagerDao();
+        }
+        return managerDao;
+    }
+
+    public void index() {
         HashMap<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("user", "eric");
+        jsonMap.put("user", "miguelangel");
         jsonMap.put("postDate", new Date());
         jsonMap.put("message", "trying out Elasticsearch");
         IndexRequest indexRequest = new IndexRequest("posts")
-                .id("1").source(jsonMap).opType(DocWriteRequest.OpType.CREATE);;
+                .id("3").source(jsonMap).opType(DocWriteRequest.OpType.CREATE);;
         try {
             IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
         } catch (IOException ex) {
             Logger.getLogger(ManagerDao.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public String get() throws Exception{
-        GetRequest getRequest = new GetRequest(
-        "posts", 
-        "1"); 
-            GetResponse getResponse = client.get(getRequest, RequestOptions.DEFAULT);
-            return getResponse.getSourceAsString();    
+
+    public String get() throws Exception {
+        GetRequest getRequest = new GetRequest("posts", "3");
+        GetResponse getResponse = client.get(getRequest, RequestOptions.DEFAULT);
+        return getResponse.getSourceAsString();
     }
-    
-    public void close(){
+
+    public void updatePosts() {
+        HashMap<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("postDate", new Date());
+        jsonMap.put("message", "daily update");
+        UpdateRequest updateRequest = new UpdateRequest("posts", "3")
+                .doc(jsonMap);
+        try {
+            UpdateResponse updateResponse = client.update(updateRequest, RequestOptions.DEFAULT);
+        } catch (IOException ex) {
+            Logger.getLogger(ManagerDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void close() {
         try {
             client.close();
         } catch (IOException ex) {
--- src/main/java/vista/Vista.java
+++ src/main/java/vista/Vista.java
@@ -1,18 +1,41 @@
 package vista;
 
 import controller.ManagerDao;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  *
  * @author Eric Ribas;
  */
 public class Vista {
+    private static ManagerDao managerDao;
     
-    public static void main(String[] args) throws Exception{
-        ManagerDao mD = new ManagerDao();
-        mD.index();
+    public static void main(String[] args) {
+        menuConsola();
+       // mD.index();
         String get = mD.get();
+        System.out.println(get);
+        mD.updatePosts();
+        get = mD.get();
         System.out.println(get);
         mD.close();
     }
+
+    public static void menuConsola() {
+            Boolean response = false;
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            managerDao = ManagerDao.getManagerDao();
+            do {
+                try {
+                    String answer = br.readLine();
+                    /*response = hotelManager.managerMenu(answer);
+                    if (!response.equals("false") && !response.equals("true")) {
+                        System.out.println(Color.messageMaker(Color.getColorByMessage(response.split(" ")[0]), response));
+                    }*/
+                } catch (IOException | NumberFormatException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            } while (!response);
+        }
 }
