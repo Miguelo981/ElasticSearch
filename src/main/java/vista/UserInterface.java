201f33ce99205e43732c09c0180e1beabf7d7f04
alu2018240 <lorito20@hotmail.es>
2020-02-12 17:39:56 +0100

    Implementacion del menu inicial e index del Dao

---
+++ src/main/java/vista/UserInterface.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import controller.ManagerDao;
+
+/**
+ *
+ * @author alu2018240
+ */
+public class UserInterface {
+    private ManagerDao managerDao;
+
+    public UserInterface(ManagerDao managerDao) {
+        this.managerDao = managerDao;
+    }
+    
+    
+    
+    
+}
