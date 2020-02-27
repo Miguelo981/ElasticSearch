d3397cd264788c6479374c8f1edf3b904137d7a3
alu2018240 <lorito20@hotmail.es>
2020-02-27 18:44:56 +0100

    Modificaciones del DAO

--- src/main/java/controller/ManagerDao.java
+++ src/main/java/controller/ManagerDao.java
@@ -3,23 +3,27 @@ package controller;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
+import modelo.Empleado;
 import org.apache.http.HttpHost;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchStatusException;
-import org.elasticsearch.action.DocWriteRequest;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.document.DocumentField;
+import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.rest.RestStatus;
-import org.elasticsearch.index.query.QueryBuilders.*;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 public class ManagerDao {
 
@@ -27,9 +31,7 @@ public class ManagerDao {
     RestHighLevelClient client;
 
     private ManagerDao() {
-        client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost("localhost", 9200, "http"))
-        );
+        client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
     }
 
     public static ManagerDao getManagerDao() {
@@ -52,16 +54,46 @@ public class ManagerDao {
         return false;
     }
 
-    public String get(GetRequest getRequest) throws Exception {
+    public Map<String, DocumentField> get(GetRequest getRequest) throws Exception {
         String s;
         try {
             GetResponse getResponse = client.get(getRequest, RequestOptions.DEFAULT);
             s = getResponse.getSourceAsString();
+            //client = ESclient.getInstant();
+            //client.get
+            /*GetResponse response = client.prepareGet("user", "tenth", "1")
+                    .setOperationThreaded(false)
+                    .get();*/
+            if (getResponse != null) {
+                //Map<String,DocumentField> FieldsMap
+                return getResponse.getFields();
+            }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
-            s = "";
         }
-        return s;
+        return null;
+    }
+
+    public Empleado getEmpleado(GetRequest getRequest) throws Exception {
+        GetResponse response = client.get(getRequest, RequestOptions.DEFAULT);
+        System.out.print(response.getSourceAsString());
+        String user = (String) response.getField("user").getValue();
+        String name = (String) response.getField("name").getValue();
+        String surname = (String) response.getField("surname").getValue();
+        String phone = (String) response.getField("phone").getValue();
+        String dni = (String) response.getField("dni").getValue();
+        return new Empleado(user, name, surname, phone, dni);
+
+    }
+
+    public Empleado getEmpleado() {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("userss");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder(); 
+        MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder("user", "Omar"); 
+        searchSourceBuilder.query(matchQueryBuilder);
+        System.out.println(searchRequest.source(searchSourceBuilder));
+        return null;
     }
 
     public boolean checkUserExists(String userName) {
@@ -88,7 +120,7 @@ public class ManagerDao {
     }
 
     public void delete() {
-        DeleteRequest request = new DeleteRequest("posts", "1");
+        DeleteRequest request = new DeleteRequest("users", "1");
         try {
             DeleteResponse deleteResponse = client.delete(
                     request,
--- src/main/java/vista/Vista.java
+++ src/main/java/vista/Vista.java
@@ -5,7 +5,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Empleado;
 import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.index.IndexRequest;
 
 /**
@@ -71,7 +75,7 @@ public class Vista {
         jsonMap.put("phone", InputAsker.askString("Phone number: ", 8));
         jsonMap.put("dni", InputAsker.askDNI("DNI: "));
         int id = 0;
-        IndexRequest indexRequest = new IndexRequest("users").id(Integer.toString(id)).source(jsonMap).opType(DocWriteRequest.OpType.CREATE);
+        IndexRequest indexRequest = new IndexRequest("userss").id(Integer.toString(id)).source(jsonMap).opType(DocWriteRequest.OpType.CREATE);
         if (managerDao.index(indexRequest)) {
             System.out.println("Usuario " + user + " creado con exito!");
         }
@@ -79,6 +83,12 @@ public class Vista {
     }
 
     private static void login() {
-
+        try {
+            managerDao.getEmpleado();
+            Empleado empleado = managerDao.getEmpleado(new GetRequest("userss", "0"));
+            System.out.print(empleado.toString());
+        } catch (Exception ex) {
+            Logger.getLogger(Vista.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
